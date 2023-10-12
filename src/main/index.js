47543c366570a75b7eeaa0c98ba17a11521ebdd7
YangWenlong123 <496192903@qq.com>
2023-10-12 09:18:59 +0800

    feat: update main.js

--- src/main/index.js
+++ src/main/index.js
@@ -8,7 +8,7 @@ import { getElectronVersion } from './version'
 function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 900,
+        width: 1200,
         height: 670,
         show: false,
         autoHideMenuBar: true,
